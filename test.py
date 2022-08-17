d0a525441a3462c14405a477982e368031dd4c74
jonnathanjumalh <jonnathanjuma@lahaus.com>
2022-08-17 18:44:39 -0500

    test7

---
+++ test.py
@@ -0,0 +1,13 @@
+import redis
+
+def test_redis_basic():
+    redis_url = 'redis://localhost:6379'
+    print(f'Redis Connection url from env_var: {redis_url}')
+    redis_conn = redis.from_url(redis_url)
+
+    # set value for a key
+    redis_conn.set('test_basic', 123)
+
+    # retrieve value
+    retrieved_val = redis_conn.get('test_basic')
+    print(f'Retrieved value: {retrieved_val}')
